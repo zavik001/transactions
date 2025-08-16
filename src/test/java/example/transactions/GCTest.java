82543c2b0da6fbf338013183f814ab07372c1075
Golem Urras <golemurr@in-b9.nsk.21-school.ru>
2025-08-16 23:26:32 +0700

    feat: add GC tests part2

--- src/test/java/example/transactions/GCTest.java
+++ src/test/java/example/transactions/GCTest.java
@@ -1,5 +1,7 @@
 package example.transactions;
 
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -8,18 +10,45 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.junit.jupiter.api.Test;
 import example.transactions.examples.helpers.Person;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 class GCTest {
+    @Test
+    void t1() {
+        List<GarbageCollectorMXBean> gcBeans = ManagementFactory.getGarbageCollectorMXBeans();
+
+        for (GarbageCollectorMXBean gcBean : gcBeans) {
+            log.info("""
+
+                    Имя GC {}
+                    Количество польных GC {}
+                    Суммарное время работы GC {}
+                    Тип GC {}
+
+                    """, gcBean.getName(), gcBean.getCollectionCount(), gcBean.getCollectionTime(),
+                    gcBean.getClass().getName());
+        }
+
+        Runtime runtime = Runtime.getRuntime();
+        log.info("""
+
+                runtime.maxMemory {}
+                runtime.freeMemory {}
+                runtime.totalMemory {}
+
+                """, runtime.maxMemory(), runtime.freeMemory(), runtime.totalMemory());
+    }
 
     @Test
-    void t1() throws InterruptedException, ExecutionException {
+    void t2() throws InterruptedException, ExecutionException {
         ExecutorService excutor = Executors.newFixedThreadPool(12);
 
         List<List<Person>> persons = new ArrayList<>();
         for (int i = 0; i < 150; i++) {
             Future<List<Person>> call = excutor.submit(() -> {
                 List<Person> list = new ArrayList<>();
-                for (int j = 0; j < 1_000/* _000 */; j++) {
+                for (int j = 0; j < 1_000_000; j++) {
                     list.add(new Person("name", 100));
                 }
                 return list;
@@ -34,7 +63,7 @@ class GCTest {
         for (int i = 0; i < 150; i++) {
             Future<List<Person>> call = excutor.submit(() -> {
                 List<Person> pers = new ArrayList<>();
-                for (int j = 0; j < 1_000/* _000 */; j++) {
+                for (int j = 0; j < 1_000_000; j++) {
                     pers.add(new Person("name", 100));
                 }
                 return pers;
@@ -49,7 +78,7 @@ class GCTest {
         for (int i = 0; i < 150; i++) {
             Future<List<Person>> call = excutor.submit(() -> {
                 List<Person> pers = new ArrayList<>();
-                for (int j = 0; j < 1_000/* _000 */; j++) {
+                for (int j = 0; j < 1_000_000; j++) {
                     pers.add(new Person("name", 100));
                 }
                 return pers;
@@ -61,5 +90,8 @@ class GCTest {
         Thread.sleep(10000);
 
         excutor.shutdown();
+        Thread.sleep(10000);
+        System.gc();
+        Thread.sleep(10000);
     }
 }
